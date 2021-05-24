18cb698c68956acde80d079c1b814e79502ec6c9
AnJunmeng <851843636@qq.com>
2021-05-24 12:12:15 +0800

    Add project files.

---
+++ ClientDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClientDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client cd = new Client();
+
+            Console.WriteLine("客户端连接服务器成功");
+            cd.connectServer();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ClientDemo/client/Client.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Threading;
+using LitJson;
+public class Client
+{
+    Socket socket;
+
+    IPEndPoint iep;
+
+    public Client()
+    {
+
+    }
+
+    public void connectServer()
+    {
+        try
+        {
+
+       
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            
+            iep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 30000);
+
+            socket.Connect(iep);
+
+
+            JsonData jd = new JsonData();
+            jd["code"] = 1;
+            jd["result"] = 4;
+            byte[] jsbyte = Encoding.UTF8.GetBytes(jd.ToString());
+
+            socket.Send(jdbyte);
+
+
+            while(true)
+            {
+                Thread.Sleep(1000);
+                socket.Send(bytes);
+      
+            }
+
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("客户端连接服务器失败");
+        }
+
+ 
+    }
+}
